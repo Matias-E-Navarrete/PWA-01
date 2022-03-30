3fc8a03e2f8b8d587ea053ce3331e649a5065857
Matias-E-Navarrete <matias.navarreteutn@gmail.com>
2022-03-30 19:42:23 -0300

    feat: Add objects and functions

--- funciones/funciones.ts
+++ funciones/funciones.ts
@@ -1,5 +1,12 @@
 // function
+function nombreDeFuncion(parametro1, parametro2) {
+    return parametro1 + parametro2
+}
 
+const result = nombreDeFuncion(2, 3)
 
 // arrow functions
+
+const reult2 = param1 => param1 + 10;
+
 
--- objetos/objetos.ts
+++ objetos/objetos.ts
@@ -1,7 +1,18 @@
 
 // Literal Objects
 
+const jhon = {
+    name: 'Jhon',
+    age: 28
+}
+
+
+
 // Destructuring
+// ... <= Spread Operator
+const jhon2 = { ...jhon, eyes: 'green' }
 
 // Properties
+jhon.age = 29;
+
 
